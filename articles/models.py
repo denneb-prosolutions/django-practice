164d1f50a144c39fea9204df8d5cf38396f3296c
denneb-prosolutions <denneb.prosolutions@gmail.com>
2022-03-23 15:48:28 +0800

    11 tutorials

--- articles/models.py
+++ articles/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.forms import DateTimeField
+
 
 # Create your models here.
-class Acticles(models.Model):
+class Articles(models.Model):
     article_name = models.CharField(max_length=60)
     article_description = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
