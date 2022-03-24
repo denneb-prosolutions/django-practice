23a82c2f2f1ca4629052304ed38b3541a650a5b8
denneb-prosolutions <denneb.prosolutions@gmail.com>
2022-03-24 13:26:34 +0800

    day 2 practices

--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Articles
+from .models import Books
 
 # Create your views here.
 def denneb_articles(request):
-    return render(request, "articles/denneb_articles.html")
+    books = Books.objects.all().order_by('datetime')
+    return render(request, "articles/denneb_articles.html" , {"books": books})
 
 def home(request):
     return render(request, "articles/home.html")
